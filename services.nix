7998a1fbf3a9a0ae4852965c708d9ad7beff9205
Jyri-Matti Lähteenmäki <jyri-matti@lahteenmaki.net>
2016-05-21 23:41:15 +0300

    added znc and (fast)cgi

--- services.nix
+++ services.nix
@@ -3,10 +3,14 @@
 {
   imports = [
     ./nginx.nix
+    ./znc.nix
   ]; 
 
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "no";
   
   services.nginx.enable = true;
+  services.fcgiwrap.enable = true;
+
+  services.znc.enable = true;
 }
---
+++ znc.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.znc.confOptions = {
+    modules = [ "adminlog" "log" ];
+    userModules = [ "buffextras" ];
+    userName = "jyri-matti";
+    nick = "Jyppe";
+    passBlock = ''
+      <Pass password>
+	Method = sha256
+	Hash = 6151fda6cd44dce9da48ba8a47a0f80c16feb4127ade769a5da3e2c4b712a9bf
+	Salt = lxKmezuI2KAxbzaOVVpp
+      </Pass>
+
+      <Network ircnet>
+        IRCConnectEnabled = true
+        Server = openirc.snt.utwente.nl 6667
+      </Network>
+    '';
+    port = 6667;
+  };
+}
