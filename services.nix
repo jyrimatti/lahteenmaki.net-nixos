db14e6e008ae77ece04b719612ec73818f79c563
Jyri-Matti Lähteenmäki <jyri-matti@lahteenmaki.net>
2016-05-25 23:20:30 +0300

    added cron and mail

--- services.nix
+++ services.nix
@@ -4,11 +4,27 @@
   imports = [
     ./nginx.nix
     ./znc.nix
-  ]; 
+  ];
 
-  services.openssh.enable = true;
-  services.openssh.permitRootLogin = "no";
-  
+  services.postfix = {
+    enable = true;
+    setSendmail = true;
+  };
+
+  services.cron = {
+    enable = true;
+    mailto = "jyri-matti@localhost";
+    systemCronJobs = [
+      "0 * * * *    jyri-matti source /etc/profile && /var/www/stiebel/simplify.sh /var/www/stiebel/data"
+      "1-59 * * * * jyri-matti source /etc/profile && /var/www/stiebel/collect.sh $(cat /home/jyri-matti/stiebel-user) $(cat /home/jyri-matti/stiebel-pass) /var/www/stiebel/data"
+    ];
+  };
+
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "no";
+  };
+
   services.nginx.enable = true;
   services.fcgiwrap.enable = true;
 
