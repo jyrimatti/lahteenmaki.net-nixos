d052f0d8f6d5742071eaf9dce3b116ed715853ec
Jyri-Matti Lähteenmäki <jyri-matti@lahteenmaki.net>
2018-09-27 21:50:26 +0300

    upgrade nixos and fix broken dns config

--- configuration.nix
+++ configuration.nix
@@ -19,13 +19,12 @@
   swapDevices = [ { device = "/swap"; } ];
 
   networking.enableIPv6 = false;
-  networking.interfaces.enp0s3.ip4 = [ { address = "146.185.139.29"; prefixLength = 24; } ];
+  networking.interfaces.enp0s3.ipv4.addresses = [ { address = "146.185.139.29"; prefixLength = 24; } ];
   networking.defaultGateway = "146.185.139.1";
-  networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
 
   networking.hostName = "lahteenmaki.net";
   networking.firewall.allowedTCPPorts = [ 80 443 6667 ];
-  networking.extraHosts = "37.136.254.85 aurinkofarmi";
+  networking.extraHosts = "176.93.30.166 aurinkofarmi";
 
   i18n = {
     consoleFont = "Lat2-Terminus16";
@@ -75,7 +74,8 @@
   users.defaultUserShell = "/run/current-system/sw/bin/zsh";
   programs.zsh.enable = true;
 
-  system.stateVersion = "16.03";
+  system.stateVersion = "18.03";
+  system.autoUpgrade.enable = true;
 
   nix.gc = {
     automatic = true;
